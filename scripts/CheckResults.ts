dda7843135ca5b59b8d586ef95e0bc5b5824a0a5
Karen J Yang <karenjyang.1@gmail.com>
2022-11-20 13:50:32 -0600

    add code

--- scripts/CheckResults.ts
+++ scripts/CheckResults.ts
@@ -1,5 +1,35 @@
+
+
+import * from "./utils";
+import { Wallet } from "ethers";
+import { TokenizedBallots } from "../typechain-types";
+// dotenv.config()
+// import * as dotenv from "dotenv";
+// dotenv.config()
+
+
+
 async function checkResults() {
-    // start here
+    // TODO
+    
+    let tokenContract: TokenizedBallots;
+    
+    // function from utils.ts
+    tokenContract.SetUpSigner();
+
+    // function from utils.ts
+    tokenContract.ballotContract(signer: Wallet);
+
+    // function from utils.ts
+    tokenContract.tokenContract(signer: Wallet);
+
+    // read winner name
+    const winnerName = await tokenContract.winnerName();
+    console.log(`winner name: $(winnerName})`);
+
+    // read winning proposal
+    const winningProposal = tokenContract.sol.winningProposal();
+    console.log(`winning proposal: ${winningProposal}`);
 
 }
 
