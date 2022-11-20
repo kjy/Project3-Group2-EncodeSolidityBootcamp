f785ff87ecfc069ab9ecc7ba052ff27e70562ee0
Akshar <akshar.1006@gmail.com>
2022-11-20 11:35:22 -0500

    refactor util

--- scripts/CheckVotePower.ts
+++ scripts/CheckVotePower.ts
@@ -2,6 +2,10 @@ import { SetupSigner } from "./utils";
 
 async function checkVotePower() {
     const [ballotContract, _] = await SetupSigner();
+
+    const voter = process.argv.slice(2);
+    const votePower = await ballotContract.votePower(voter[0]);
+    console.log(votePower)
 }
 
 checkVotePower().catch((e) => {
