a6aee893b06b2868e22788ac1f979820cb7ab3a7
Akshar Rawal <akshar.1006@gmail.com>
2022-11-19 21:30:37 -0500

    mint ready

--- scripts/Mint.ts
+++ scripts/Mint.ts
@@ -1,21 +1,20 @@
 import { ethers } from "ethers";
-import { SetupSigner } from "./constants";
+import { SetupSigner } from "./utils";
 
 const MINT_TOKENS = ethers.utils.parseEther("1000");
 
 async function giveVotingTokens() {
     const [ballotContract, tokenContract] = await SetupSigner();
 
-    const participants = process.argv.slice(2);
+    const args = process.argv.slice(2);
+    if (args.length != 2) throw new Error("Expecting exactly 2 args");
+    const voter = args[0];
+    const mintAmount = ethers.utils.parseEther(args[1]);
 
-    if (participants.length <= 0) throw new Error("Not enough parameters");
-
-    participants.forEach(async p => {
-        const mintTx = await tokenContract.mint(p, MINT_TOKENS);
-        await mintTx.wait()
-        let voterTokenBalance = await tokenContract.balanceOf(p);
-        console.log(`After mint voter ${p} has a total of ${voterTokenBalance} decimal units`);
-    });
+    const mintTx = await tokenContract.mint(voter, MINT_TOKENS);
+    await mintTx.wait()
+    let voterTokenBalance = await tokenContract.balanceOf(voter);
+    console.log(`After mint voter ${voter} has a total of ${voterTokenBalance} decimal units`);
 }
 
 giveVotingTokens().catch((e) => {
