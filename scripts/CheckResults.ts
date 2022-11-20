cb9f5d357080f8aac5abdfe332f92f958a28095b
Karen J Yang <karenjyang.1@gmail.com>
2022-11-20 15:51:10 -0600

    add changes to files

--- scripts/CheckResults.ts
+++ scripts/CheckResults.ts
@@ -1,39 +1,49 @@
 
-
 import * from "./utils.ts";
+import * from "./constants.ts";
 import { Wallet } from "ethers";
 import { TokenizedBallots } from "../typechain-types";
-// dotenv.config()
-// import * as dotenv from "dotenv";
-// dotenv.config()
+dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config()
 
 
 
 async function checkResults() {
     // TODO
     
-    let tokenContract: TokenizedBallots;
+    let tokenizedBallotContract: TokenizedBallots;
     
     // function from utils.ts
-    tokenContract.SetUpSigner();
+    tokenizedBallotContract.SetUpSigner();
 
     // function from utils.ts
-    tokenContract.ballotContract(signer: Wallet);
+    tokenizedBallotContract.ballotContract(signer: Wallet);
 
-    // function from utils.ts
-    tokenContract.tokenContract(signer: Wallet);
 
     // read winner name
-    const winnerName = await tokenContract.winnerName();
+    const winnerName = await tokenizedBallotContract.winnerName();
     console.log(`winner name: $(winnerName})`);
 
     // read winning proposal
-    const winningProposal = tokenContract.sol.winningProposal();
+    const winningProposal = tokenizedBallotContract.sol.winningProposal();
     console.log(`winning proposal: ${winningProposal}`);
 
 }
 
-checkResults().catch((e) => {
-    console.log(e);
+checkResults().catch((error) => {
+    console.error(error);
     process.exitCode = 1;
-})
+});
+
+
+/*
+
+To run file,   yarn run ts-node --files ./scripts/CheckResults.ts ./scripts/CheckResults.ts  "ballotContractAddress"
+
+
+From constants.ts
+export const ballotContractAddress: string = "";
+export const tokenContractAddress: string = "0x284A7042be8749c1b3A35509F27ebb09c2737956";
+
+*/
--- scripts/Vote.ts
+++ scripts/Vote.ts
@@ -2,39 +2,37 @@ import * from "./utils.ts";
 import * from "./constants.ts";
 import { Wallet } from "ethers";
 import { TokenizedBallots } from "../typechain-types";
-// dotenv.config()
-// import * as dotenv from "dotenv";
-// dotenv.config()
+dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config()
 
 
 async function vote() {
     // TODO
     
-    let tokenContract: TokenizedBallots;
+    let tokenizedBallotContract: TokenizedBallots;
     // function from utils.ts
-    tokenContract.SetUpSigner();
+    tokenizedBallotContract.SetUpSigner();
 
     // function from utils.ts
-    tokenContract.ballotContract(signer: Wallet);
+    tokenizedBallotContract.ballotContract(signer: Wallet);
+    
+    const args = process.argv.slice(2);
 
-    // function from utils.ts
-    tokenContract.tokenContract(signer: Wallet);
-    
-    
     const proposal = args[0];  // proposal index
     const amount = args[1];   // amount
     console.log(`Voting for ${vote}`);
 
-    const tx = await tokenContract.vote(proposal, amount);
+    const tx = await tokenizedBallotContract.vote(proposal, amount);
     
-    vote().catch((e) => {
-        console.log(e);
+    vote().catch((error) => {
+        console.error(error);
         process.exitCode = 1;
     });
 
 };
 
-// yarn run ts-node --files scripts/Vote.ts proposalIndexNum amount
+// To run file, yarn run ts-node --files scripts/Vote.ts proposalIndexNum amount
 
     /*
     From TokenizedBallots.sol
@@ -45,9 +43,39 @@ async function vote() {
         spentVotingPower[msg.sender] += amount;
     }
 
-    from constants.ts file
+    From constants.ts file
     
     export const ballotContractAddress: string = "";
     export const tokenContractAddress: string = "0x284A7042be8749c1b3A35509F27ebb09c2737956";
 
+
+    From utils.ts
+
+    import { ethers, Wallet } from "ethers";
+import { Ballot, Ballot__factory, MyToken, MyToken__factory } from "../typechain-types";
+import { ballotContractAddress, tokenContractAddress } from "./constants";
+import * as dotenv from "dotenv";
+dotenv.config()
+
+export async function SetupSigner(): Promise<Wallet> {
+    const provider = ethers.getDefaultProvider("goerli", { etherscan: process.env.ETHERSCAN_API_KEY })
+    let wallet: Wallet;
+
+    if (process.env.MNEMONIC != "") {
+        wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "")
+    } else {
+        wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "")
+    }
+    return wallet.connect(provider)
+}
+
+export async function ballotContract(signer: Wallet): Promise<Ballot> {
+    const ballotFactory = new Ballot__factory(signer);
+    return await ballotFactory.attach(ballotContractAddress);
+}
+
+export async function tokenContract(signer: Wallet): Promise<MyToken> {
+    const tokenFactory = new MyToken__factory(signer);
+    return await tokenFactory.attach(tokenContractAddress);
+}
     */
